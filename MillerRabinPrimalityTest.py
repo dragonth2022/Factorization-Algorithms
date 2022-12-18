cebeed3c8195002c1ac983c328c442618fbb173e
dragonth2022 <56974673+dragonth2022@users.noreply.github.com>
2022-12-18 22:53:29 +0800

    Update MillerRabinPrimalityTest.py

--- MillerRabinPrimalityTest.py
+++ MillerRabinPrimalityTest.py
@@ -10,43 +10,30 @@ import math
 def MillerRabin(n):
     
     reallyrandomint = secrets.SystemRandom()
-    
     m = n-1
     
-    #We have to treat even numbers as well as 1 as special cases
-    #Otherwise, n - 1 will be odd and this causes problems subsequently
-    
     if n < 1:
-        
         verdict = 'Error message: Please enter a POSITIVE integer.'
-        
+    
     elif n == 1:
-        
         verdict = False
         
     elif n == 2:
         verdict = True
         
     elif n%2 == 0:
-        
         verdict = False
         
     else:
-        #Function that finds the highest power of 2 that divides an integer m
         def extract2(m):
             a = m-1
             b = ~a
             c = m&b
             return c
-        #k is the highest power of 2 that divides m = n-1
+
         k = extract2(m)
-        #e is such that 2 power e = k
         e = int(math.log(k,2))
-        
-        
         failcount= 0
-        
-        
         
         for r in range(10):
             
