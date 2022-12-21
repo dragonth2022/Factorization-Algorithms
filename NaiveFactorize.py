0e4cee3a4027cc42c348f3699ef1a626c238472d
Zhao Yufan <56974673+yufan196884@users.noreply.github.com>
2022-12-21 19:44:20 +0800

    Update NaiveFactorize.py

--- NaiveFactorize.py
+++ NaiveFactorize.py
@@ -4,10 +4,11 @@ Created on Sun Dec 18 22:47:40 2022
 
 @author: Zhao Yufan
 """
+#naive factorization. simply checks all integers less than sqrt(n)
+
 import math
 
 
-#naive factorization. simply checks all integers less than sqrt(n)
 def naive_factorize(n):
     maxfactor = math.floor(math.sqrt(n))+1
     for number in range(2,maxfactor+1):
